cb92d314e1b247241203aaf43622ff78b7e2c602
Shannon M. Hauck <shannonmariehauck@gmail.com>
2023-02-16 17:21:13 -0500

    basic seeds for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+5000.times do |i|
+  Contact.create!(first_name: "first_name_#{i}", last_name: "last_name_#{i}", personal_email: "personal_email_#{i}", mobile_phone: "cellphone_#{i}")
+end
+
+AssessmentLevel.create!(level: "1")
+AssessmentLevel.create!(level: "2")
+AssessmentLevel.create!(level: "3")
+AssessmentLevel.create!(level: "HR")
+AssessmentLevel.create!(level: "MGR")
+
+EventType.create(name: "OC Meeting")
+EventType.create(name: "Protest - Solidarity")
+EventType.create(name: "Protest - Our Union")
+EventType.create(name: "Protest - Walkout")
+EventType.create(name: "Union Day")
+
+JobLevel.create(level: "99")
+JobLevel.create(level: "1")
+JobLevel.create(level: "2")
+JobLevel.create(level: "3")
+JobLevel.create(level: "4")
+JobLevel.create(level: "5")
+JobLevel.create(level: "6")
+JobLevel.create(level: "7")
+JobLevel.create(level: "8")
+JobLevel.create(level: "10")
+JobLevel.create(level: "11")
+JobLevel.create(level: "12")
+
+JobTitle.create(title: "HRBP")
+JobTitle.create(title: "ICQA Programmer")
+JobTitle.create(title: "Project Manager")
+JobTitle.create(title: "Program Manager")
+JobTitle.create(title: "SDE")
+JobTitle.create(title: "SDE II")
+JobTitle.create(title: "Sr. SDE")
+
+SocialMediaType.create!(name: "dA")
+SocialMediaType.create!(name: "Discord")
+SocialMediaType.create!(name: "Facebook")
+SocialMediaType.create!(name: "Mastodon")
+SocialMediaType.create!(name: "Reddit")
+SocialMediaType.create!(name: "Twitter")
