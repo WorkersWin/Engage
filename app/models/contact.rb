24235a4dfc23470872a0a878f6afed49f40f6202
sleepy.pen9078 <sleepy.pen9078@fastmail.com>
2024-05-13 21:01:52 -0500

    Rename association to make it a little easier to read

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,12 @@
 class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  has_many :assessor_assessments, class_name: 'Assessment', foreign_key: 'assessor_id'
-  has_many :assessee_assessments, class_name: 'Assessment', foreign_key: 'assessee_id'
   has_many :contact_took_trainings
+
   scope :current_assessment, -> { order('assessed_on desc').first }
+
+  has_many :given_assessments, class_name: 'Assessment', foreign_key: 'assessor_id'
+  has_many :received_assessments, class_name: 'Assessment', foreign_key: 'assessee_id'
 
   def display_name
     "#{first_name} #{last_name}"
