7d2f8b0b15c34d8f0b90734b74bd9fcf44fa7569
Shannon M. Hauck <shannonmariehauck@gmail.com>
2025-04-16 19:38:03 -0400

    typo in organizations for testing

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,7 +3,7 @@ class OrganizationsController < ApplicationController
 
   # GET /organizations or /organizations.json
   def index
-    @organizations = Organization.all
+    @organizations = Organization.all.order(:name)
   end
 
   # GET /organizations/1 or /organizations/1.json
