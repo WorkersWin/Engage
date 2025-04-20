d9d816ea67e037a6a8dc892d38ab1d82d7c03a88
Shannon M. Hauck <shannonmariehauck@gmail.com>
2025-04-19 23:53:35 -0400

    migrated from bootstrap to bulma css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
 
 import "trix"
 import "@rails/actiontext"
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -116,11 +116,11 @@
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/docs/extras/bootstrap
-require 'pagy/extras/bootstrap'
+# require 'pagy/extras/bootstrap'
 
 # Bulma extra: Add nav, nav_js and combo_nav_js helpers and templates for Bulma pagination
 # See https://ddnexus.github.io/pagy/docs/extras/bulma
-# require 'pagy/extras/bulma'
+require 'pagy/extras/bulma'
 
 # Pagy extra: Add the pagy styled versions of the javascript-powered navs
 # and a few other components to the Pagy::Frontend module.
